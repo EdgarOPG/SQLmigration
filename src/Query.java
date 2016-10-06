0de0c55377f5ec62c9ef25ed1442c506d776ee05
EdgarOPG <eopg95@gmail.com>
2016-10-06 05:25:01 -0600

    Get Methods

--- src/Query.java
+++ src/Query.java
@@ -2,6 +2,9 @@
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import sqlmigration.conexiones.Conexion;
 
 /*
@@ -17,16 +20,56 @@ import sqlmigration.conexiones.Conexion;
 public class Query {
     public static void main(String[] args) {
         Query q = new Query();
-        q.getDirectorById();
+        q.getTablenames("HR");
+        q.getNameColums("EMPLOYEES");
+        
     }
+    
+public List<String> getTablenames(String user){
+    try {
+        ArrayList<String> tablesnamesList = new ArrayList<>();
+        Statement st = Conexion.getInstance().getCon().createStatement();
+        ResultSet rs = st.executeQuery("SELECT table_name FROM all_all_tables WHERE owner = '" + user + "'");
+        while (rs.next()) {
+            tablesnamesList.add(rs.getString(1));
+        }
+        for(int x=0; x < tablesnamesList.size(); x++) {
+            System.out.println(tablesnamesList.get(x));
+        }
+        return tablesnamesList;
+    } catch (SQLException ex) {
+        System.out.println(ex);
+        return null;
+    }
+}
+
+public List<String> getNameColums(String tablename){
+    try {
+        ArrayList<String> columsList = new ArrayList<>();
+        Statement st = Conexion.getInstance().getCon().createStatement();
+        ResultSet rs = st.executeQuery("SELECT column_name FROM all_tab_columns "
+                + "WHERE table_name = '"+ tablename + "'");
+        while (rs.next()) {
+            columsList.add(rs.getString(1));
+        }
+        for(int x=0; x < columsList.size(); x++) {
+            System.out.println(columsList.get(x));
+        }
+        return columsList;
+    } catch (SQLException ex) {
+        System.out.println(ex);
+        return null;
+    }
+}
+    
+    
+    
     public void getDirectorById() {
-        Integer i = 0;
         try {
             Statement st = Conexion.getInstance().getCon().createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM EMPLOYEES");
+            ResultSet rs = st.executeQuery("SELECT * FROM all_all_tables WHERE owner = 'HR'");
             while (rs.next()) {
-                System.out.println(i + " " + rs.getString(2));
-                i++;
+                System.out.println(rs.getMetaData().getColumnCount());
             }
         } catch (SQLException ex) {
             System.out.println(ex);
