4fb80d60f091f218f6577f5146622901887fed3b
EdgarOPG <eopg95@gmail.com>
2016-10-07 03:46:58 -0600

    advances in create method

--- src/Query.java
+++ src/Query.java
@@ -3,7 +3,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import static javafx.scene.input.KeyCode.T;
 import sqlmigration.conexiones.Conexion;
 
 /*
@@ -17,13 +19,14 @@ import sqlmigration.conexiones.Conexion;
  * @author eopg9
  */
 public class Query {
+    
+    //TODO Con string format crear el create tables
     public static void main(String[] args) {
         Query q = new Query();
         
         for (int i = 0; i < q.getTablenames("HR").size(); i++) {
             System.out.println(q.getTablenames("HR").get(i));
-            System.out.println(q.getNameColums(q.getTablenames("HR").get(i)));
-//            System.out.println(q.getRows(q.getTablenames("HR").get(i)));
+            System.out.println(fieldsToQuery(q.getNameColums(q.getTablenames("HR").get(i))));
         }
     }
     
@@ -47,8 +50,11 @@ public List<String> getNameColums(String tablename){
     try {
         ArrayList<String> columsList = new ArrayList<>();
         Statement st = Conexion.getInstance().getCon().createStatement();
-        ResultSet rs = st.executeQuery("        SELECT column_name FROM all_tab_columns "
-                                        + "WHERE table_name = '"+ tablename + "'");
+        ResultSet rs = st.executeQuery("SELECT column_name || ' ' "
+                                        + "|| DATA_TYPE || ' ' || NULLABLE "
+                                        + "FROM all_tab_cols "
+                                        + "WHERE table_name = '"
+                                        + tablename + "'");
         while (rs.next()) {
             columsList.add(rs.getString(1));
         }
@@ -74,4 +80,14 @@ public List<String> getRows(String tablename){
     }
 }
         
+public static String fieldsToQuery(List<String> fields) {
+            String campos = "";
+            List<String> fieldsList = fields;
+            String [] fieldsArray = fieldsList.toArray(new String[fieldsList.size()]);
+            for (String field : fieldsArray) {
+                campos = String.format("%s, %s", campos, field);   
+            }
+            campos = campos.substring(1);
+            return campos;
+            }
 }
